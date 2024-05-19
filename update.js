53c2aede9c9f2ee01018ac0ccbe1a8b3e2c16d75
EMON-BHAI <sharifvau4@gmail.com>
2024-05-20 00:14:12 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,8 +1,8 @@
 {
   "name": "EMon-BHai-BOT",
-  "description": "Emon system update and some error fix",
-  "new command": "No",
-  "update": "yes",
+  "description": "Emon system update",
+  "new command": "No added",
+  "update": "Version Update",
   "owner": "https://www.facebook.com/EMon.BHai.FACEBOOK",
   "main" : "\nReplit: r\n\nGitHub: g",
   "version": "1.0.1"
